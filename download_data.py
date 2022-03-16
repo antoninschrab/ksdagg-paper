99a0179f4a16e824aad3e15c448b42ac924d6b83
antoninschrab <61871753+antonin-s@users.noreply.github.com>
2022-03-16 12:15:00 +0000

    fix download_data.py

--- download_data.py
+++ download_data.py
@@ -5,9 +5,38 @@ This data has been obtained by running the generate_data_rbm.py and generate_dat
 """
 
 from pathlib import Path
-from google_drive_downloader import GoogleDriveDownloader as gdd
+import requests, zipfile
+from tqdm import tqdm
 
-gdd.download_file_from_google_drive(
-    file_id="1CSj36IEW9x5_CCbiNXqBhqVqofiaGE0D", dest_path="./data.zip", unzip=True
-)
+if Path('data').exists():
+    raise ValueError("Directory ./data already exists!")
+
+print('Starting download')
+url = 'https://drive.google.com/u/0/uc?id=1CSj36IEW9x5_CCbiNXqBhqVqofiaGE0D&export=download&confirm=t'
+filename = "data.zip"
+response = requests.get(url, stream=True)
+# progress bar
+total_size_in_bytes= int(response.headers.get('content-length', 0))
+block_size = 1024
+progress_bar = tqdm(total=total_size_in_bytes, unit='iB', unit_scale=True)
+with open(filename, 'wb') as file:
+    for data in response.iter_content(block_size):
+        progress_bar.update(len(data))
+        file.write(data)
+progress_bar.close()
+if total_size_in_bytes != 0 and progress_bar.n != total_size_in_bytes:
+    raise ValueError("Error: download failed. Try again.")
+print('Download completed')
+
+print('Starting unzip')
+with zipfile.ZipFile(filename) as zf:
+     for member in tqdm(zf.infolist(), desc='Extracting '):
+         try:
+             zf.extract(member, './')
+         except zipfile.error as e:
+             pass
+print('Unzip completed')
+
+# delete .zip file
 Path("./data.zip").unlink()
+
